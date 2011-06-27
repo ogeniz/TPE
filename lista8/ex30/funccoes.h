fde883b06fa4923976d0a51ed961b0eb95265f77
Ogeniz Façanha Costa <ogeniz@gmail.com>
2011-06-27 18:18:39 -0300

    Multiplicação de matrizes utilizando funções, alocação dinâmica de memória para as matrizes.

---
+++ lista8/ex30/funccoes.h
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#ifndef FUNCCOES_H
+#define FUNCCOES_H
+
+#define FALSE 0
+#define TRUE 1
+
+typedef unsigned short ushort;
+typedef unsigned char uchar;
+
+typedef struct
+{
+   int **address;
+   ushort row;
+   ushort col;
+}Matriz;
+
+
+void ler_matriz(Matriz *);
+void preenche_matriz(Matriz *);
+void inicializa_matriz(Matriz *);
+int **aloca_matriz(const ushort *, const ushort *);
+void multiplica_matrizes(Matriz *, Matriz *, Matriz *);
+void imprime_matriz(const Matriz *);
+void libera_matriz(int **, const ushort *);
+
+#endif
---
+++ lista8/ex30/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "funccoes.h"
+
+int main(void)
+{
+  Matriz matriz_A, matriz_B, matriz_Mult;
+  multiplica_matrizes(&matriz_A,&matriz_B,&matriz_Mult);
+  return 0;
+}
