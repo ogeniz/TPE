3caf3766f9dd495da6ec140ebd50a78b33fb6c3f
Ogeniz Façanha Costa <ogeniz@gmail.com>
2011-06-29 19:11:34 -0300

    Mudança na função multiplica_matrizes, agora a função é completamente auto contida, ou seja não recebe e não retorna nada.

--- lista8/ex30/main.c
+++ lista8/ex30/main.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "funccoes.h"
+#include "functions.h"
 
 int main(void)
 {
-  Matriz matriz_A, matriz_B, matriz_Mult;
-  multiplica_matrizes(&matriz_A,&matriz_B,&matriz_Mult);
+  multiplica_matrizes();
   return 0;
 }
